3113380f2a40b109ddb1ff17e298ca118e66b7f6
admin <admin>
2019-09-15 00:27:08 -0300

    Documentatiom

--- src/sequence.py
+++ src/sequence.py
@@ -2,19 +2,38 @@ import exrex
 
 
 class Sequence:
+    """
+    A sequence is a group of numbers and / or letters that will form a single character. This character will be changed
+    using the given pattern, so if you need to create a string, for example with the following letters C3P6, the first
+    time the "next" and "get" methods are executed, the result will be the letter C " The next run will return the
+    digit "3", the next run the letter "P" and finally the next run will return the digit "6".
+    The purpose of this class is therefore to return one character at a time, using the character set entered in
+    the sequence, and each of the characters entered will be returned at each execution of the "next" and "get" methods.
+    When all characters have been returned, a next execution of the "next" and "get" methods will restart the sequence.
+    """
     def __init__(self, sequence, parent=None):
         self.sequence = sequence
         self.index = -1
         self.parent = parent
 
     def send(self, *args, **kwargs):
+        """
+        Send a message for this class.
+        """
         raise NotImplemented()
 
     def send_to_parent(self):
+        """
+        Send a message for the parent.
+        """
         if self.parent:
             self.parent.send(self)
 
     def set(self, value):
+        """
+        Set the current sequence.
+        :param value: the next sequence.
+        """
         old = self.get()
         if old == value:
             return
@@ -26,14 +45,21 @@ class Sequence:
                 raise Exception("Impossible to set the value")
 
     def previous(self):
-        pass
+        raise NotImplemented()
 
     def get(self):
+        """
+        :return: the current sequence's value.
+        """
         if self.index == -1:
             self.index = 0
         return self.sequence[self.index]
 
     def next(self):
+        """
+        Advance for the next sequence.
+        :return: self
+        """
         self.index += 1
         if self.index < len(self.sequence):
             return self
@@ -45,6 +71,33 @@ class Sequence:
 
 
 class Sequences(Sequence):
+    """
+    A list of sequences whose values will be returned incrementally according to the entered pattern. One such sequence
+    pattern could be, for example, as follows:
+
+    AA00
+    AA01
+    AA02
+    AA99
+    AE01
+    AE02
+    AE99
+    AI01
+    ...
+    UU98
+    UU99
+    AA00
+
+    To create a pattern consisting of 4 characters, with letters being sequences of vowels and
+    numbers formed by digits 0-9, just do the following:
+
+    s = Sequences (
+        Sequence ("AEIOU"),
+        Sequence ("AEIOU"),
+        Sequence ("0123456789"),
+        Sequence ("0123456789"),
+    )
+    """
     def __init__(self, sequence, parent=None):
         super().__init__(sequence, parent)
         self.indexes = []
@@ -81,7 +134,7 @@ class Sequences(Sequence):
         return result
 
     def previous(self):
-        pass
+        raise NotImplemented()
 
     def next(self):
         if len(self.indexes) > 0:
@@ -227,6 +280,31 @@ class DnaSequence(Sequences):
 
 
 def factory(pattern):
+    """
+    Creates a sequence pattern using a string consisting of constants and regular expressions to represent the sequence
+    of values. To create a sequence with the following pattern (including the constant "2019"):
+
+    A-2019-00
+    A-2019-01
+    A-2019-02
+    A-2019-99
+    B-2019-00
+    B-2019-01
+    B-2019-99
+    C-2019-00
+    ...
+    Z-2019-00
+    Z-2019-01
+    Z-2019-98
+    Z-2019-99
+
+    Just run the following code
+
+    s = factory ("[A-Z];-2019-;[0-9];[0-9]")
+
+    :param pattern: the pattern to create the sequence.
+    :return: the instance of the class Sequences.
+    """
     if pattern is None:
         return None
 
